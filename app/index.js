1e6def13bd485bb1805c8596e7ca99753f69f796
Jhonn Alvarez <jsalvarezmolina@student.fullsail.edu>
2025-01-22 20:49:38 -0800

    feat: Add root GET route to return status and metadata
    - Implemented a simple GET route on the root ("/") path.
    - Returns a 200 status with a JSON message and metadata including the HTTP method.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json())
+
+//localhost:3000/
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "GET - root", metadata: { hostname: req.method, 
+    },
+});
+});
 
 module.exports = app;
