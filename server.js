cdeea2a8aeb1e31ce9e35fb4ae2ba2055ff244d9
Jhonn Alvarez <jsalvarezmolina@student.fullsail.edu>
2025-01-22 20:09:02 -0800

    Set up environment variables and server initialization
    - Configured dotenv to load environment variables from .env file
    - Set up server to listen on a dynamic port using process.env.PORT
    - Commented out MongoDB connection and related code
    - Added logging to display port and environment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const dotenv = require("dotenv").config();
-const connectDB = require("./app/db/config");
+// const connectDB = require("./app/db/config");
 const app = require("./app");
 
 //Connect to MongoDB
-connectDB();
+// connectDB();
 
 const PORT = process.env.PORT || 3000;
-console.log("process.env.PORT", Process.env.PORT);
+console.log("process.env.PORT", process.env.PORT);
 
 app.listen(PORT, () => {
     console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`);
